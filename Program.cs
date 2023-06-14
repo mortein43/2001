9b85d7c2c1326e63009d5e29ce270cc28dda2e2b
Eizenheim <Eizenheim@DESKTOP-6PHV0Q0>
2023-06-14 18:29:26 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2001
+{
+    
+    class MyForm : Form
+    {
+        public int i = 0;
+        public MyForm(string caption)
+        {
+            Text = caption;
+            Click += new EventHandler(ClickHandler);
+            
+        }
+        public void ClickHandler(Object sender, EventArgs e)
+        {
+            i++;
+            MessageBox.Show($"Стільки раз натиснули на формі - {i.ToString()}.");
+        }
+    }
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            /*Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());*/
+            Application.Run(new MyForm("Мій заголовок"));
+        }
+    }
+}
